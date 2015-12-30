9714fb67445de68021ffa1544b00463c29464060
sivakumarg2 <sivakumar.g@lnttechservices.com>
2015-12-30 19:56:18 +0530

    Kudu installation modified

--- deploy.cmd
+++ deploy.cmd
@@ -38,12 +38,11 @@ IF NOT DEFINED NEXT_MANIFEST_PATH (
   )
 )
 
-IF NOT DEFINED KUDU_SYNC_CMD (
   :: Install kudu sync
   echo Installing Kudu Sync
   call npm install kudusync -g --silent
   IF !ERRORLEVEL! NEQ 0 goto error
-)
+
 
 :: Locally just running "kuduSync" would also work
 SET KUDU_SYNC_CMD=%appdata%\npm\kuduSync.cmd
